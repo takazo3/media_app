776b454defa2a71bad0b1af4e745b8005ba33c7e
takazo3 <info@re-zero-jp.com>
2024-08-23 00:29:11 +0900

    Update subproject commit hash

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/sanctum/csrf-cookie', [CsrfCookieController::class, 'show']);
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
--- server/routes/web.php
+++ server/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/sanctum/csrf-cookie', [CsrfCookieController::class, 'show']);
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
